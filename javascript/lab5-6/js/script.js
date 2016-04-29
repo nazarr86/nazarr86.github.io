ede9a7af45597f460eeec2a8ac1635a4709ee729
Nazar Riabokin <nazar.o.r.86@gmail.com>
2016-04-29 15:03:05 +0300

    Finished lab5-6

--- javascript/lab5-6/js/script.js
+++ javascript/lab5-6/js/script.js
@@ -2,26 +2,41 @@ var container = document.createElement('div');
 container.classList.add('container');
 var parentForContainer = document.body;
 parentForContainer.appendChild(container);
-container.style.width = '100%';
+container.style.width = '400px';
 container.style.margin = '0 auto';
 container.style.marginTop = '140px';
 
-var timerDisplay = document.createElement('p');
+var timerDisplay = document.createElement('div');
 timerDisplay.classList.add('timer-display');
 var parentForContent = document.querySelector('.container');
 parentForContent.appendChild(timerDisplay);
-timerDisplay.innerHTML = "00:00:00";
+timerDisplay.style.width = '100%';
 timerDisplay.style.border = '5px solid #000';
-timerDisplay.style.width = '415px';
-timerDisplay.style.fontSize = '60px';
-timerDisplay.style.fontWeight = 'bold';
-timerDisplay.style.borderRadius = '15px';
+timerDisplay.style.background = '#e1e3ff';
+timerDisplay.style.borderRadius = '30px';
+timerDisplay.style.marginBottom = '20px';	
 
-var timerDisplayMilliseconds = document.createElement('span');
-timerDisplayMilliseconds.classList.add('timer-display__milliseconds');
-var parentForSpan = document.querySelector('.timer-display');
-parentForSpan.appendChild(timerDisplayMilliseconds);
-timerDisplayMilliseconds.innerHTML = '  000';
+var timerDisplayHours = document.createElement('p');
+timerDisplayHours.classList.add('timer-display-hours');
+parentForTimer = document.querySelector('.timer-display');
+parentForTimer.appendChild(timerDisplayHours);
+timerDisplayHours.innerHTML = "00:00:00";
+// timerDisplayHours.style.margin = '0';
+timerDisplayHours.style.margin = '0px 0px -20px';
+timerDisplayHours.style.fontSize = '70px';
+timerDisplayHours.style.fontWeight = 'bold';
+timerDisplayHours.style.textAlign = 'center';
+
+
+var timerDisplayMillis = document.createElement('p');
+timerDisplayMillis.classList.add('timer-display__millis');
+parentForTimer.appendChild(timerDisplayMillis);
+timerDisplayMillis.innerHTML = "0";
+timerDisplayMillis.style.textAlign = 'right';
+timerDisplayMillis.style.fontSize = '30px';
+timerDisplayMillis.style.fontWeight = 'bold';
+timerDisplayMillis.style.paddingRight = '50px';
+timerDisplayMillis.style.margin = '0px 0px -5px';
 
 var startPause = document.createElement('button');
 startPause.classList.add('btn');
@@ -30,6 +45,8 @@ startPause.classList.add('btn-lg');
 startPause.innerHTML = 'Start';
 startPause.setAttribute('onclick', 'startStopTimer()');
 parentForContent.appendChild(startPause);
+startPause.style.color = '#000';
+startPause.style.width = '120px';
 
 var clear = document.createElement('button');
 clear.classList.add('btn');
@@ -38,6 +55,10 @@ clear.classList.add('btn-lg');
 clear.innerHTML = 'Clear';
 clear.setAttribute('onclick', 'clearTimer()');
 parentForContent.appendChild(clear);
+clear.style.color = '#000';
+clear.style.float = 'right';
+clear.style.width = '120px';
+
 
 var flag = 0;
 var time = 0;
@@ -55,7 +76,8 @@ function startStopTimer(){
 	time = 0;
 	flag = 0;
 	startPause.innerHTML = "Start";
-	timerDisplay.innerHTML = "00:00:00";
+	timerDisplayHours.innerHTML = "00:00:00";
+	timerDisplayMillis.innerHTML = "0";
 }
 
 function calculTime(){
@@ -65,7 +87,7 @@ function calculTime(){
 			var hours = Math.floor(time / 10 / 60 / 60);
 			var mins = Math.floor(time / 10 / 60);
 			var secs = Math.floor(time / 10 % 60);
-			var millsecs = Math.floor(time % 10);
+			var millsecs = time % 10;
 			 if (hours < 10){
 			 	hours = "0" + hours;
 			 }
@@ -75,8 +97,8 @@ function calculTime(){
 			 if (secs < 10){
 			 	secs = "0" + secs;
 			 }
-			timerDisplay.innerHTML = hours + ":" + mins + ":" + secs + ":" + millsecs;
-			timerDisplayMilliseconds.innerHTML = "  " + millsecs + "0";
+			timerDisplayHours.innerHTML = hours + ":" + mins + ":" + secs;
+			timerDisplayMillis.innerHTML = "  " + millsecs;
 			calculTime();
 		}, 100)
 	}
