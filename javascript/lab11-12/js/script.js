45422ca095ae7104ea35d9c27f255d27f785dac6
Nazar Riabokin <nazar.o.r.86@gmail.com>
2016-06-24 13:48:12 +0300

    Added templating

--- javascript/lab11-12/js/script.js
+++ javascript/lab11-12/js/script.js
@@ -14,8 +14,13 @@ $(function () {
         },
         car_discription: "Суперкар с мощным двигателем и умопомрачительным дизайном",
         car_plus: "Основные приемущества: ",
-        car_advantages: ['изысканный стиль в неповторной форме', 'стильный двигатель V8 FSI', 'мощный привод quattro']
-
+        car_advantages: ['изысканный стиль в неповторной форме', 'стильный двигатель V8 FSI', 'мощный привод quattro'],
+        car_seller_telephone: "Контактный телефон поставщика",
+        car_seller_number: "+380970000123",
+        car_seller_address: "Сайт поставщика в Украине",
+        car_seller_site: "http://www.audi.ua/",
+        feedback: "Фидбек",
+        feedback_text: "Это отличная тачка"
     }
 
     var content = tmpl(html, data);
