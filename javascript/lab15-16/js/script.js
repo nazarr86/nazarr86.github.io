fa87185bbc49c3c8d7f6d83a948d50f3a4219f1c
Nazar Riabokin <nazar.o.r.86@gmail.com>
2016-06-30 11:11:34 +0300

    Finished lab 15-16

--- javascript/lab15-16/js/script.js
+++ javascript/lab15-16/js/script.js
@@ -1,3 +1,4 @@
+//SEARCH FORM
 $(function () {
 	
 	$('.btn').on('click', search);
@@ -31,3 +32,52 @@ $(function () {
         $('.search').val('');
 }
 })
+
+//CLASSES
+
+function Human(name, age, gender, height, weight) {
+    this.name = name;
+    this.age = age;
+    this.gender = gender;
+    this.height = height;
+    this.weight = weight;
+}
+
+Human.prototype.do = function() {
+  console.log('Я сейчас ' + this.work)
+}
+
+function Worker(name, age, gender, height, weight, workplace, sale) {
+	Human.apply(this, arguments);
+	this.workplace = workplace;
+	this.sale = sale
+	this.work = 'работаю!!!';
+}
+
+function Student(name, age, gender, height, weight, study, scholarship) {
+	Human.apply(this, arguments);
+	this.study = study;
+	this.scholarship = scholarship;
+	this.work = 'смотрю теливизор';
+}
+
+Worker.prototype = new Human();
+Worker.prototype.constructor = Worker;
+Student.prototype = new Human();
+Student.prototype.constructor = Student;
+
+var andrey = new Worker('Andrey', 26, 'male', 190, 85, 'factory', 3000);
+console.log(andrey);
+andrey.do();
+
+var irina = new Worker('Irina', 24, 'female', 170, 55, 'web-studio', 4000);
+console.log(irina);
+irina.do();
+
+var max = new Student('Max', 23, 'male', 192, 90, 'university', 100);
+console.log(max);
+max.do();
+
+var sveta = new Student('Sveta', 28, 'female', 168, 50, 'courses', 0);
+console.log(sveta);
+sveta.do();
