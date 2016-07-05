12e3cda95cb94294ef82dd53b8f4c17fc041b2f2
Nazar Riabokin <nazar.o.r.86@gmail.com>
2016-07-05 14:26:19 +0300

    Modifed lab 21-22

--- javascript/lab21-22/gulpfile.js
+++ javascript/lab21-22/gulpfile.js
@@ -1,16 +1,40 @@
 var gulp = require('gulp'),
     concatCSS = require('gulp-concat-css'),
     cleanCSS = require('gulp-clean-css'),
-    watch = require('gulp-watch');
+    watch = require('gulp-watch'),
+    autoprefixer = require('gulp-autoprefixer'),
+    livereload = require('gulp-livereload'),
+    connect = require('gulp-connect');
 
-
-gulp.task('default', function () {
-    return gulp.src('src/css/**/*.css')
-        .pipe(concatCSS("style.min.css"))
-        .pipe(cleanCSS())
-        .pipe(gulp.dest('dist/css/'));
+//server connect
+gulp.task('connect', function() {
+  connect.server({
+    root: 'dist',
+    livereload: true
+  });
 });
 
-gulp.task('watch', function(){
-    gulp.watch('src/css/**/*.css', ['default'])
+//css
+gulp.task('css', function () {
+    return gulp.src('src/css/**/*.css')
+        .pipe(concatCSS("style.min.css"))
+        .pipe(autoprefixer('last 2 version', '> 1%', 'ie 9'))
+        .pipe(cleanCSS())
+        .pipe(gulp.dest('dist/css/'))
+        .pipe(connect.reload()); //перезагрузить браузер так как были применени изменения
+});
+
+//html
+gulp.task('html', function(){
+    gulp.src('dist/index.html')
+    .pipe(connect.reload());
 })
+
+//watch
+gulp.task('watch', function(){
+    gulp.watch('src/css/**/*.css', ['css'])
+    gulp.watch('dist/index.html', ['html'])
+})
+
+//default task
+gulp.task('default', ['connect', 'html', 'css', 'watch']);
